bd01305e7adccbd877588ab2600ff5f1b953e743
Igor Malinov <malinov.igor@gmail.com>
2024-04-15 20:37:39 +0300

    Добавлена исчезающая clearEditText

--- app/src/main/java/ru/startandroid/develop/sprint8v3/SearchActivity.kt
+++ app/src/main/java/ru/startandroid/develop/sprint8v3/SearchActivity.kt
@@ -1,11 +1,44 @@
 package ru.startandroid.develop.sprint8v3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val editTextToBeCleared = findViewById<EditText>(R.id.edit_text)
+        val clearEditText = findViewById<ImageView>(R.id.clear_text)
+
+        clearEditText.setOnClickListener {
+            editTextToBeCleared.setText("")
+        }
+
+        clearEditText.visibility = View.INVISIBLE
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    clearEditText.visibility = View.INVISIBLE
+                } else {
+                    clearEditText.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        editTextToBeCleared.addTextChangedListener(textWatcher)
+
+
+
     }
 }
