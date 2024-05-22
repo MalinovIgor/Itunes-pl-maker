0a3eff5476157f7f679d4e08faeefadf74298211
Igor Malinov <malinov.igor@gmail.com>
2024-05-22 21:15:45 +0300

    to revise TrackAdapter

--- app/src/main/java/ru/startandroid/develop/sprint8v3/TrackAdapter.kt
+++ app/src/main/java/ru/startandroid/develop/sprint8v3/TrackAdapter.kt
@@ -7,18 +7,19 @@ import ru.startandroid.develop.sprint8v3.TrackViewHolder
 
 class TrackAdapter(private val trackList: List<Track>) : RecyclerView.Adapter<TrackViewHolder>()
      {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
-        return TrackViewHolder(itemView)
-    }
+         var tracks = ArrayList<Track>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder = TrackViewHolder(parent)
+    //{ хз, может быть надо будет это вернуть..
+    //val itemView =
+       //     LayoutInflater.from(parent.context).inflate(R.layout.item_track, parent, false)
+      //  return TrackViewHolder(itemView)
+   // }
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
-        val currentTrack = trackList[position]
-        holder.bind(currentTrack)
+        holder.bind(tracks.get(position))
     }
 
     override fun getItemCount(): Int {
-        return trackList.size
+        return tracks.size
     }
 }
