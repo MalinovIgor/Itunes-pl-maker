d57443f79a6e4f7a4cb8bce50518522a3c498efe
Igor Malinov <malinov.igor@gmail.com>
2024-09-19 19:54:28 +0300

    adapter to add

--- app/src/main/java/ru/startandroid/develop/sprint8v3/library/ui/LibraryActivity.kt
+++ app/src/main/java/ru/startandroid/develop/sprint8v3/library/ui/LibraryActivity.kt
@@ -5,51 +5,37 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.RelativeLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.startandroid.develop.sprint8v3.R
+import ru.startandroid.develop.sprint8v3.databinding.ActivityLibraryBinding
 
 class LibraryActivity : AppCompatActivity() {
 
-    private var relativeLayout: RelativeLayout? = null
+    private lateinit var binding: ActivityLibraryBinding
+    private lateinit var tabMediator: TabLayoutMediator
 
-    private var lastButtonIndex = 0
-    private var lastButtonId: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mediateka)
-    }
-        private fun addNewButton() {
-            val newButton = createNewButton(lastButtonIndex, lastButtonId)
+        binding = ActivityLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            relativeLayout?.addView(newButton)
+        binding.viewPager.adapter = LibraryPagerAdapter(
+            fragmentManager = supportFragmentManager,
+            lifecycle = lifecycle,
+        )
 
-            ++lastButtonIndex
-            lastButtonId = newButton.id
-        }
-
-        private fun createNewButton(
-            currentButtonIndex: Int,
-            previousButtonId: Int,
-        ): View {
-            val isFirstButton = currentButtonIndex == 0
-
-            return Button(this).also { button ->
-                button.text = "Button # $lastButtonIndex"
-
-                button.id = View.generateViewId()
-
-                button.layoutParams = RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                ).apply {
-                    this.marginEnd = 10
-                    if (isFirstButton) {
-                        this.addRule(RelativeLayout.ALIGN_PARENT_END)
-                    } else {
-                        this.addRule(RelativeLayout.START_OF, previousButtonId)
-                    }
-                }
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = getString(R.string.favorites)
+                1 -> tab.text = getString(R.string.playlists)
             }
         }
+        tabMediator.attach()
+    }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
 }
